b35804e40da13a6a64d40d98ff8f1446423bbc0a
NicollySilva <47272660+NicollySilva@users.noreply.github.com>
2020-12-04 11:00:24 -0300

    Commit Inicial
    Subindo código da calculadora criado em sala.

---
+++ calculadoraNicolly.js
@@ -0,0 +1,51 @@
+var numero1 = 0
+var numero2 = 0
+var operador = ""
+
+
+function calcular(numero1, operador, numero2) {
+    switch (operador) {
+
+        case "+": return numero1 + numero2
+        case "-": return numero1 - numero2
+        case "*": return numero1 * numero2
+        case "/": return numero1 / numero2
+        case "%": return numero1 % numero2
+    }
+}
+
+function setarNumero(numeroDigitado) {
+    var textoVisor = document.getElementById("visor").innerText
+    if (!operador) {
+        numero1 = textoVisor + numeroDigitado
+    } else{
+        numero2 = numero2+ ""+numeroDigitado
+    }
+    document.getElementById("visor").innerHTML += numeroDigitado
+}
+
+function setarOperator (operadorDigitado) {
+   if(!operador){
+       operador = operadorDigitado
+   } else{
+       setarResultado()
+       operador = operadorDigitado
+   }
+    document.getElementById("visor").innerHTML+= operadorDigitado
+    
+}
+
+function setarResultado (){
+    var resultado
+    if(numero1 && numero2 && operador){
+    resultado = calcular(parseFloat(numero1), operador, parseFloat(numero2))
+    } else{
+        resultado = 0
+    } 
+    document.getElementById("visor").innerHTML = resultado
+    numero1 = resultado
+    numero2 = 0
+    operador = ""
+
+}
+
